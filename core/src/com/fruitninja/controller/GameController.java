c4bb4cc7e6f5008396849d8f36195dc79b5d76f5
polyndrom <polyndromdev@gmail.com>
2019-11-09 02:09:13 +1000

    Some changes

--- core/src/com/fruitninja/controller/GameController.java
+++ core/src/com/fruitninja/controller/GameController.java
@@ -7,25 +7,26 @@ import com.badlogic.gdx.utils.TimeUtils;
 import com.fruitninja.FruitNinjaMain;
 import com.fruitninja.model.fruit.BananaFruit;
 import com.fruitninja.model.fruit.Fruit;
+import com.fruitninja.model.fruit.PineappleFruit;
 import com.fruitninja.model.fruit.WatermelonFruit;
 import com.fruitninja.view.GameScreen;
 import com.fruitninja.view.LoseMenuScreen;
-
-import java.util.Random;
 
 public class GameController {
 
     private long lastSpawnTime = 0;
 
     private void spawnFruit() {
-        float randomX = MathUtils.random(0f, Gdx.graphics.getWidth() - Fruit.WIDTH);
+        float randomX = MathUtils.random(0f, Gdx.graphics.getWidth() - Fruit.DEFAULT_WIDTH);
 
         Fruit randomFruit;
-        int fruitProbability = MathUtils.random(0 , 7);
-        if (fruitProbability >= 0 && fruitProbability < 4) {
+        int fruitProbability = MathUtils.random(0 , 10);
+        if (fruitProbability >= 0 && fruitProbability <= 4) {
             randomFruit = new WatermelonFruit(randomX, Gdx.graphics.getHeight());
-        } else {
+        } else if (fruitProbability >= 5 && fruitProbability <= 7){
             randomFruit = new BananaFruit(randomX, Gdx.graphics.getHeight());
+        } else {
+            randomFruit = new PineappleFruit(randomX, Gdx.graphics.getHeight());
         }
 
         GameScreen.fruits.add(randomFruit);
@@ -42,15 +43,18 @@ public class GameController {
             FruitNinjaMain.game.setScreen(new LoseMenuScreen());
         }
 
-        if (TimeUtils.millis() - lastSpawnTime > MathUtils.random(300, 1500)) spawnFruit();
+        if (TimeUtils.millis() - lastSpawnTime > MathUtils.random(700, 2000)) spawnFruit();
 
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
             float x = Gdx.input.getX();
             float y = Gdx.graphics.getHeight() - Gdx.input.getY();
             for (Fruit fruit : GameScreen.fruits) {
                 if (fruit.getBounds().contains(x, y)) {
-                    score++;
-                    GameScreen.fruits.remove(fruit);
+                    fruit.health--;
+                    if (fruit.health <= 0) {
+                        score++;
+                        GameScreen.fruits.remove(fruit);
+                    }
                     break;
                 }
             }
@@ -58,7 +62,7 @@ public class GameController {
 
         for (Fruit fruit : GameScreen.fruits) {
             fruit.getBounds().y -= fruit.getSpeed() * Gdx.graphics.getDeltaTime();
-            if (fruit.getBounds().y + Fruit.HEIGHT < 0) {
+            if (fruit.getBounds().y + Fruit.DEFAULT_HEIGHT < 0) {
                 health--;
                 GameScreen.fruits.remove(fruit);
                 break;
