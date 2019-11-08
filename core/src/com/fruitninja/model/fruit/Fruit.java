83e672b90d77339d06388de72b8454c087803a8b
polyndrom <polyndromdev@gmail.com>
2019-11-09 02:07:23 +1000

    Added pineapple fruit & some changes

--- core/src/com/fruitninja/model/fruit/Fruit.java
+++ core/src/com/fruitninja/model/fruit/Fruit.java
@@ -1,47 +1,58 @@
 package com.fruitninja.model.fruit;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Fruit {
 
-    public static final float WIDTH = 48f;
-    public static final float HEIGHT = 48f;
+    public static final float DEFAULT_WIDTH = 48f;
+    public static final float DEFAULT_HEIGHT = 48f;
 
     public static Texture WATERMELON_TEXTURE = null;
     public static Texture BANANA_TEXTURE = null;
+    public static Texture PINEAPPLE_TEXTURE = null;
 
     public static void loadTextures() {
         WATERMELON_TEXTURE = new Texture("fruits/watermelon.png");
         WATERMELON_TEXTURE.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         BANANA_TEXTURE = new Texture("fruits/banana.png");
         BANANA_TEXTURE.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        PINEAPPLE_TEXTURE = new Texture("fruits/pineapple.png");
+        PINEAPPLE_TEXTURE.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
     }
 
     public static void unloadTextures() {
         WATERMELON_TEXTURE.dispose();
         BANANA_TEXTURE.dispose();
+        PINEAPPLE_TEXTURE.dispose();
     }
 
     private Rectangle bounds;
-    private Sprite sprite;
+    private Texture texture;
     private float speed;
+    private int max_health;
+    public int health;
 
-    public Fruit(float x, float y, Texture texture, float speed) {
-        this(new Rectangle(x, y, WIDTH, HEIGHT), new Sprite(texture), speed);
+    public void setSize(int width, int height) {
+        bounds.width = width;
+        bounds.height = height;
     }
 
-    public Fruit(Rectangle bounds, Sprite sprite, float speed) {
+    public Fruit(float x, float y, Texture texture, float speed, int max_health) {
+        this(new Rectangle(x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT), texture, speed, max_health);
+    }
+
+    public Fruit(Rectangle bounds, Texture texture, float speed, int max_health) {
         this.bounds = bounds;
-        this.sprite = sprite;
+        this.texture = texture;
         this.speed = speed;
+        this.max_health = max_health;
+        this.health = max_health;
     }
 
     public void render(SpriteBatch batch) {
-        this.sprite.setBounds(bounds.x, bounds.y, bounds.width, bounds.height);
-        this.sprite.draw(batch);
+        batch.draw(texture, bounds.x, bounds.y, bounds.width, bounds.height);
     }
 
     public Rectangle getBounds() {
